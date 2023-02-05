950198cdf7b4ce73ae42a9daaf02189b2ab597b6
Tang Qiming <missingjs@outlook.com>
2023-02-05 10:23:58 +0800

    install pre-commit

--- server/colax/server/server.py
+++ server/colax/server/server.py
@@ -9,7 +9,13 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/consul-host")
 async def consul_host():
     consul_host = os.environ.get("COLAX_CONSUL_HOST")
     return {"consul_host": consul_host}
+
+
+@app.get("/health")
+async def health():
+    return {"message": "OK"}
