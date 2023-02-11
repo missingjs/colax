e2f032213b8be0a53a0509ca45b0b13cd52a4239
Tang Qiming <missingjs@outlook.com>
2023-02-11 20:20:00 +0800

    project management script

---
+++ bin/script.files/project.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+usage()
+{
+    cat << EOF
+usage: colax project <command> [options]
+
+Commands:
+    create <name>    Create new project with given name.
+EOF
+    exit 1
+}
+
+cmd=$1
+
+[ -z $cmd ] && usage
+
+function create_project()
+{
+    local prj_name=$1
+    local prj_file="${prj_name}.conf"
+    [ -e $prj_file ] && { echo "Error: $prj_file already exist"; exit 3; }
+    cat >$prj_file <<EOF
+project_name = $prj_name
+EOF
+    echo "Project $prj_name created"
+}
+
+case $cmd in
+    create)
+        proj_name=$2
+        [ -z $proj_name ] && usage
+        create_project $proj_name
+        ;;
+    *)
+        usage
+        ;;
+esac
