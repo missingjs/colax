df43c359c9e1cdbfe953babdef9da8a41e2e4bd8
Qiming Tang <qiming.tang@tusen.ai>
2023-02-22 18:31:08 +0800

    logging config for colax server

--- server/run-locally.sh
+++ server/run-locally.sh
@@ -2,4 +2,9 @@
 
 export COLAX_CONSUL_ADDR="localhost:8500"
 
-uvicorn colax.server.server:app --host 0.0.0.0 --port 6733
+uvicorn colax.server.server:app \
+    --host 0.0.0.0 \
+    --port 6733 \
+    --access-log \
+    --log-level debug \
+    --log-config ./logging.json
